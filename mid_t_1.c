8d9f6b92fd865e500a5a809ecd236886a75f3044
LEEHYUNDONG <l.steve10@yahoo.com>
2017-10-12 20:01:42 +0900

    mid term problem 1

---
+++ mid_t_1.c
@@ -0,0 +1,84 @@
+
+#include<stdio.h>
+
+
+int main()
+{
+
+    char str[80];
+    int n, i, j, tot = 0, st;
+    int num[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int flag = 0;
+
+    
+    
+    printf("Input String: "); fflush(stdout);
+    
+    i = 0;
+
+    while((str[i]=getchar()) != '\n')
+    {   
+        i++;
+    }
+            
+            printf("Number of output characters: ");
+            scanf("%d", &n);
+            st = i;
+    
+    printf("- Part String: ");
+
+    for(i = (st - n - 1);i <= st;i++)
+    {
+        putchar(str[i]);
+
+        if(str[i] >= '0' && str[i] <= '9')
+        {
+            tot += (str[i] - '0');
+        
+            for(j = 0;j < 10;j++)
+            {
+                if(str[i] == j + '0') num[j]++;
+
+            }
+        }
+
+    }
+
+    printf("\n- Total Constants: %d\n", tot);
+    
+    
+    printf("- Frequency of number repitition:");
+    
+    
+    for(j = 0;j < 10;j++)
+    {
+        if(num[j] != 0)
+        {
+            printf("(%d) : %d\t", j, num[j]);
+            flag++;
+        }
+        if((flag % 4) == 0)
+        {
+            printf("\n");
+        }
+    
+    }
+    
+    printf("\n-  Number string: ");
+
+    for(i = (st - n - 1);i = st;i++)
+    {
+        if((str[i] >= '0') && (str[i] <= '9'))
+        {
+            putchar(str[i]);
+            //printf("%c", str[i]);
+        }
+    }
+
+    printf("\n\n");
+
+
+    return(0);
+
+}
+    
