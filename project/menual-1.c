d7008eb15cec4b2dc930274374aa31b8910c8d8c
LEEHYUNDONG <l.steve10@yahoo.com>
2017-11-12 17:07:51 +0900

    project

---
+++ project/menual-1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void (*menu_1)(void)
+{
+
+    int num;
+    
+    
+    printf("--------------------------\n");
+    printf(" 1. Sign Up\n 2. Sign In\n 3. List Of User\n \n");
+    printf("--------------------------\n");
+    
+    printf("PRESS KEY : "); scanf("%d", &num);
+
+    
+    switch(num)
+    {
+
+        case 1 :
+            sign_up();      
+            break;
+
+        case 2 :
+            sign_in();
+            break;
+
+        case 3 :
+            list_user();
+            break;
+
+    }
+            
+
+        
+    return(0);
+
+}
