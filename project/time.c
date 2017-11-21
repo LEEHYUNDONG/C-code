ee99e4e49c6b2bc09ba8eefc28359aa54de53266
LEEHYUNDONG <l.steve10@yahoo.com>
2017-11-22 01:26:30 +0900

    project

---
+++ project/time.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<time.h>
+
+void main()
+{
+
+    time_t now;
+    struct tm t;
+
+
+    time(&now);
+    t = *localtime(&now);
+
+    printf("Date&Time : %4d.%d.%d %d:%d:%d\n", t.tm_year+1900, 
+    t.tm_mon+1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec);
+
+}
--- time.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<time.h>
-
-
-int time(int n_note, int n_person)
-{
-
-    while(1)
-    {
