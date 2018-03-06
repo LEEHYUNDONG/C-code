c0a3636c120d2e068ba4aaa766860ccb7c7dae61
LEEHYUNDONG <32635799+LEEHYUNDONG@users.noreply.github.com>
2018-03-06 22:49:08 +0900

    Update homew2.c

--- book/pointer/homew2.c
+++ book/pointer/homew2.c
@@ -1,62 +1,69 @@
 #include <stdio.h>
-#define ADD(x, y) ((x) + (y))
+/*#define ADD(x, y) ((x) + (y))
 #define SUB(x, y) ((x) - (y))
 #define MUL(x, y) ((x) * (y))
-
-typedef struct
+*/
+struct Complex 
 {
   int real;
   int img;  
   
-}num;
+};
+typedef struct Complex complex;
 
-void input(num *n, char ch, int *pn); //function pointer to the res function!!
+complex input(char ch, int *pn); //function pointer to the res function!!
 
 int main(void)
 {
-  num a ={0, 0};  
+  complex com;  
   int n[4];
   char ch;
      
-  printf("Input calculation of complex number(sum, sub, mul)\(equation ex)(a + bi)+(c - di) \n >");
-  scanf("%d   %d", &n[0], &n[1]);
-  scanf(" %c", &ch);
-  scanf("%d   %d", &n[2], &n[3]);
-  input(&a, ch, n);
-      
-  printf("\nResult : %d + (%di)\n", a.real, a.img);
+  printf(" Input calculation of complex number(sum, sub, mul)\n (equation ex)(a + bi)<>(c - di) \n >");
+  scanf("a : %d b :%d",&n[0], &n[1]);
+  scanf("\n<> :%c", &ch);
+  scanf("\nc : %d d : %d"&n[2], &n[3]);
   
+  input(ch, n);
+      
+  printf("\nResult : %d + %di\n", com.real, com.img);
+  
+  system("pause");
   return 0;
 
 }
 
-void input(num *n, char ch, int *pn)
+complex input(char ch, int *pn)
 {
-    
+    complex com;
     int x, y, z;
     
     if(ch == '+')
     {
-        x = ADD(pn[0], pn[2]);
-        y = ADD(pn[1], pn[3]);
+        x = pn[0] + pn[2];
+        y = pn[1] + pn[3];
            
     }
     else if(ch == '-')
     {
-        x = SUB(pn[0], pn[2]);
-        y = SUB(pn[1], pn[3]);
+        x = pn[0] - pn[2];
+        y = pn[1] - pn[3];
   
     }
     else if(ch == '*')
     {
-        x = MUL(pn[0], pn[2]);
-        y = MUL(pn[1], pn[3]);
+        x = pn[0] * pn[2];
+        y = (pn[1] * pn[3]);
         z = -y;
         x = x + z;
-        y = MUL(pn[0],pn[3]) + MUL(pn[2],pn[1]);
+        y = (pn[0]*pn[3]) + (pn[2]*pn[1]);
     
     }
-    n->real = x;
-    n->img = y;
+    com.real = x;
+    com.img = y;
+    
+    return complex;
             
 }
+
+
